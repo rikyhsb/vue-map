0e11c5ed50e3d16eeb69f4fef3fba03c3b7aee84
Riky Hasibuan <riky.hasibuan@gmail.com>
2019-10-09 14:12:20 +0700

    Initial Commit

---
+++ src/components/Map.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="map">
+      <h2>Peta Dengan VueJs dan LeafletJs</h2>
+      <div style="height: 600px; width: 100%">
+        <l-map :zoom="zoom" :center="center">
+          <l-marker :lat-lng="center"></l-marker>
+          <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+        </l-map>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 12,
+      center: [-6.93,107.60],
+      bounds: null,
+
+    };
+  }
+}
+</script>
